06395122f6b27f74f32890ad246c2f7120d5c58d
Dmitry <mitjagagin@yandex.ru>
2026-04-06 18:51:56 +1100

    feat: lesson 02 - add loop example

--- lessons/lesson_02_lists.py
+++ lessons/lesson_02_lists.py
@@ -1,9 +1,14 @@
-# Урок 2: Списки
+# Урок 2: Списки, цикл for
 # Автор: Дмитрий Гагин
-# Цель: Изучить что такое списки и для чего они нужны
+# Цель: Изучить что такое списки и для чего они нужны. Цикл for и его применение.
 
 students: list[str] = ["Bob", "John", "Michael"]
 students.append("Anna")
 
+for student in students:
+    print(student)
+
 print(students)
 print(students[0])
+
+

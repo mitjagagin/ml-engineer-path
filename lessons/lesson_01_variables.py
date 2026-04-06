323c273761caf66de9814d90413e25d0580ee7b8
Dmitry <mitjagagin@yandex.ru>
2026-04-06 18:17:40 +1100

    feat: lesson 01 - variables and type annotations

---
+++ lessons/lesson_01_variables.py
@@ -0,0 +1,15 @@
+# Урок 1: Переменные
+# Автор : Дмитрий Гагин
+# Цель : понять что такое переменные и как Python хранит данные
+
+answer: int = 42 # ответ
+floor: int = 9 # этаж
+pi: float = 3.14 # число Пи
+name: str = "Bob" # имя
+is_ml_engineer: bool = True # должность
+
+print(answer) # печатаем ответ
+print(floor) # печатаем этаж
+print(pi) # печатаем Пи
+print(name) # печатаем имя
+print(is_ml_engineer) # печатаем должность

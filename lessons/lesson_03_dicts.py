99326e554df5cbd1cc1e4431e2f3bd6544d831f0
Dmitry <mitjagagin@yandex.ru>
2026-04-07 08:47:08 +1100

    feat: lesson 03 - dictionaries

---
+++ lessons/lesson_03_dicts.py
@@ -0,0 +1,18 @@
+# Урок 3: Словари
+# Автор: Дмитрий Гагин
+# Цель: Изучить что такое словари и для чего они нужны.
+
+from typing import Union
+
+person: dict[str, Union[int, str]] = {
+    "name": "Дмитрий",
+    "age": 45,
+    "height": 185,
+    "weight": 90
+}
+
+for key, value in person.items():
+    print(f"{key}: {value}")
+
+print(person)
+print(person["name"])

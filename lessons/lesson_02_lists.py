36f13bb0dc957b4b54b8a1782ba0fe3bd2b513c6
Dmitry <mitjagagin@yandex.ru>
2026-04-06 18:39:47 +1100

    feat: lesson 02 lists

---
+++ lessons/lesson_02_lists.py
@@ -0,0 +1,9 @@
+# Урок 2: Списки
+# Автор: Дмитрий Гагин
+# Цель: Изучить что такое списки и для чего они нужны
+
+students: list[str] = ["Bob", "John", "Michael"]
+students.append("Anna")
+
+print(students)
+print(students[0])
